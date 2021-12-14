d22ffe84cf3ceb237b91cc4562870ec67358d9bc
fwend <josvanuden@gmail.com>
2021-12-14 12:51:29 +0100

    refactor

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var g = canvas.getContext("2d");
+const g = canvas.getContext("2d");
 
-var inc = 0;
+let inc = 0;
 
 function drawSpiral(len, angleIncrement) {
-    var x1 = canvas.width / 2;
-    var y1 = canvas.height / 2;
-    var angle = angleIncrement;
+    let x1 = canvas.width / 2;
+    let y1 = canvas.height / 2;
+    let angle = angleIncrement;
 
-    for (var i = 0; i < 150; i++) {
+    for (let i = 0; i < 150; i++) {
 
-        var x2 = x1 + Math.cos(angle) * len;
-        var y2 = y1 - Math.sin(angle) * len;
+        const x2 = x1 + Math.cos(angle) * len;
+        const y2 = y1 - Math.sin(angle) * len;
 
         g.strokeStyle = HSVtoRGB(i / 150, 1.0, 1.0);
         g.beginPath();
@@ -33,7 +33,7 @@ function drawSpiral(len, angleIncrement) {
 
 /* copied from stackoverflow */
 function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
+    let r, g, b, i, f, p, q, t;
 
     i = Math.floor(h * 6);
     f = h * 6 - i;
@@ -41,12 +41,12 @@ function HSVtoRGB(h, s, v) {
     q = v * (1 - f * s);
     t = v * (1 - (1 - f) * s);
     switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
     }
     return "rgb("
         + Math.round(r * 255) + ","
